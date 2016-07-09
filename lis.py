74f1a061bbc18015cbe0200779b4fea33bfec77c
Clinton Dow <clint@Clintons-MacBook-Pro.local>
2016-07-09 11:51:28 -0700

    Added parse and sub-methods

--- lis.py
+++ lis.py
@@ -1,3 +1,39 @@
+"""Implementing a Scheme interpreter in Python."""
+Symbol = str           # A Scheme Symbol equals Python str
+List = list            # A Scheme List equals a Python list
+Number = (int, float)  # A Scheme Number equals a Python int or float
+
 def tokenize(chars):
     """Convert a string of characters into a list of tokens"""
     return chars.replace('(', ' ( ').replace(')', ' ) ').split()
+
+
+def parse(program):
+    """Read a scheme expression from a string"""
+    return read_from_tokens(tokenize(program))
+
+def read_from_tokens(tokens):
+    """Read an expression from a sequence of tokens"""
+    if len(tokens) == 0:
+        raise SyntaxError('unexpected EOF while reading.')
+    token = tokens.pop(0)
+    if '(' == token:
+        L = []
+        while tokens[0] != ')':
+            L.append(read_from_tokens(tokens))
+        tokens.pop(0)  # remove ')'
+        return L
+    elif ')' == token:
+        raise SyntaxError('unexpected )')
+    else:
+        return atom(token)
+
+def atom(token):
+    """Numbers become numbers, every other token is a symbol"""
+    try:
+        return int(token)
+    except ValueError:
+        try:
+            return float(token)
+        except ValueError:
+            return Symbol(token)
