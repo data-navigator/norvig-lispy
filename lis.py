72ad8014e4a2cfc3dbb3d110dc3bf1ee48f50673
Clinton Dow <clint@Clintons-MacBook-Pro.local>
2016-07-09 12:10:09 -0700

    Added standard_env

--- lis.py
+++ lis.py
@@ -1,7 +1,51 @@
 """Implementing a Scheme interpreter in Python."""
+
+Env = dict             # An environment is a mapping of {variable: value}
 Symbol = str           # A Scheme Symbol equals Python str
 List = list            # A Scheme List equals a Python list
 Number = (int, float)  # A Scheme Number equals a Python int or float
+
+global_env = standard_env()
+
+def standard_env():
+    """An environment with some Scheme standard prodcedures"""
+    import math
+    import operator
+    env = Env()  # new dict as an Env
+    env.update(vars(math))  # sin, cos, sqrt, pi ...
+    env.update(
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.div,
+        '>': operator.gt,
+        '<': operator.lt,
+        '>=': operator.ge,
+        '<=': operator.le,
+        '=': operator.eq,
+        'abs': abs,
+        'append': operator.add,
+        'apply': apply,
+        'begin': lambda *x: x[-1],
+        'car': lambda x: x[0],
+        'cdr': lambda x: x[1:],
+        'cons': lambda x,y: [x] + y,
+        'eq?': operator.is_,
+        'equal': operator.eq,
+        'length' len,
+        'list': lambda *x: list(x)
+        'list?': lambda x: isinstance(x,list),
+        'map': map,
+        'max': max,
+        'min': min,
+        'not': operator.not_,
+        'null?': lambda x: x ==[],
+        'number?': lambda x: isinstance(x, Number),
+        'procedure?': callable,
+        'round': round,
+        'symbol?': lambda x: isinstance(x, Symbol)
+    )
+    return env
 
 def tokenize(chars):
     """Convert a string of characters into a list of tokens"""
