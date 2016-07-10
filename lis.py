5f4ce515e01ba9e08d178f45d96b9c9c21cc5235
Clinton Dow <clint@Clintons-MacBook-Pro.local>
2016-07-09 17:41:39 -0700

    added REPL

--- lis.py
+++ lis.py
@@ -122,3 +122,19 @@ def eval(x, env=global_env):
         proc = eval(x[0], env)
         args = [eval(arg, env) for arg in x[1:]]
         return proc(*args)
+
+
+def schemestr(exp):
+    """Convert a Python object back into a Scheme-readable string"""
+    if isinstance(exp, list):
+        return '(' + ' '.join(map(schemestr, exp)) + ')'
+    else:
+        return str(exp)
+
+
+def repl(prompt='lis.py>'):
+    """A prompt Read Eval Print Loop"""
+    while True:
+        val = eval(parse(input(prompt)))
+        if val is not None:
+            print(schemestr(val))
